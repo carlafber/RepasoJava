6d8449179d34463a36df22720b6f39be2eb5ba31
carlafber <carla.fueber@educa.jcyl.es>
2024-09-18 10:33:32 +0200

    Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int opcion = -1;
-        do {
-            System.out.println("MENÚ:\n  1. Abrir una nueva cuenta\n  2. Ver un listado de las cuentas disponibles." +
-                    "\n  3. Obtener los datos de una cuenta concreta.\n  4. Realizar un ingreso en una cuenta." +
-                    "\n  5. Retirar efectivo de una cuenta.\n  6. Consultar el saldo actual de una cuenta." +
-                    "\n  ---------------------------------------------\n  0. SALIR  ");
-            System.out.println("Seleccione una opción:");
-            opcion = in.nextInt();
-        } while (opcion != 0);
+        try {
+            do {
+                System.out.println("MENÚ:\n  1. Abrir una nueva cuenta\n  2. Ver un listado de las cuentas disponibles." +
+                        "\n  3. Obtener los datos de una cuenta concreta.\n  4. Realizar un ingreso en una cuenta." +
+                        "\n  5. Retirar efectivo de una cuenta.\n  6. Consultar el saldo actual de una cuenta." +
+                        "\n  ---------------------------------------------\n  0. SALIR  ");
+                System.out.println("Seleccione una opción:");
+                opcion = in.nextInt();
+                in.nextLine();
+
+                switch (opcion) {
+                    case 1:
+                }
+
+            } while (opcion != 0);
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
