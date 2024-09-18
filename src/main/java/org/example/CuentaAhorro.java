c0a323b6c65cf924f3468a8c7f2c2da01aa9052a
carlafber <carla.fueber@educa.jcyl.es>
2024-09-18 10:58:33 +0200

    Commit

--- src/main/java/org/example/CuentaAhorro.java
+++ src/main/java/org/example/CuentaAhorro.java
@@ -1,4 +1,37 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class CuentaAhorro extends CuentaBancaria {
+    double tipoInteres;
+
+    public CuentaAhorro(Persona titular, int saldo, String iban, double tipoInteres) {
+        super(titular, saldo, iban);
+        this.tipoInteres = tipoInteres;
+    }
+
+    @Override
+    public double tipoInteres() {
+        return tipoInteres;
+    }
+
+    @Override
+    public ArrayList<String> listaEntidades() {
+        return null;
+    }
+
+    @Override
+    public double comisionMantenimiento() {
+        return 0;
+    }
+
+    @Override
+    public double tipoInteresDescubierto() {
+        return 0;
+    }
+
+    @Override
+    public double maximoDescubierto() {
+        return 0;
+    }
 }
--- src/main/java/org/example/CuentaBancaria.java
+++ src/main/java/org/example/CuentaBancaria.java
@@ -1,4 +1,54 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public abstract class CuentaBancaria {
+    Persona titular;
+    int saldo;
+    String iban;
+
+    /*public CuentaBancaria() {
+        super();
+    }
+
+    public CuentaBancaria(Persona titular, int saldo, String iban) {
+        super(titular, saldo, iban);
+    }*/
+
+    public CuentaBancaria(Persona titular, int saldo, String iban) {
+        super();
+        this.titular = titular;
+        this.saldo = saldo;
+        this.iban = iban;
+    }
+
+    public Persona getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Persona titular) {
+        this.titular = titular;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public abstract double tipoInteres();
+    public abstract ArrayList<String> listaEntidades();
+    public abstract double comisionMantenimiento();
+    public abstract double tipoInteresDescubierto();
+    public abstract double maximoDescubierto();
 }
