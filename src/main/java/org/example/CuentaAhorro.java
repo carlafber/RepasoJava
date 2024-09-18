db6660a69bfd9828f9b3f0d29c490c1e55a5162a
carlafber <carla.fueber@educa.jcyl.es>
2024-09-18 10:39:55 +0200

    Commit

---
+++ src/main/java/org/example/CuentaAhorro.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class CuentaAhorro extends CuentaBancaria {
+}
---
+++ src/main/java/org/example/CuentaCorrienteEmpresa.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class CuentaCorrienteEmpresa extends CuentaCorriente {
+}
