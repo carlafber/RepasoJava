2619475d314f159a3a91b7a80d1e826719926cb0
carlafber <carla.fueber@educa.jcyl.es>
2024-09-18 11:07:03 +0200

    Commit

--- src/main/java/org/example/CuentaCorrienteEmpresa.java
+++ src/main/java/org/example/CuentaCorrienteEmpresa.java
@@ -1,4 +1,33 @@
 package org.example;
 
 public class CuentaCorrienteEmpresa extends CuentaCorriente {
+    double tipoInteresDescubierto;
+    double maximoDescubierto;
+
+    public CuentaCorrienteEmpresa(Persona titular, int saldo, String iban, double tipoInteresDescubierto, double maximoDescubierto) {
+        super(titular, saldo, iban);
+        this.tipoInteresDescubierto = tipoInteresDescubierto;
+        this.maximoDescubierto = maximoDescubierto;
+    }
+
+
+    @Override
+    public double comisionMantenimiento() {
+        return 0;
+    }
+
+    @Override
+    public double tipoInteresDescubierto() {
+        return tipoInteresDescubierto;
+    }
+
+    @Override
+    public double maximoDescubierto() {
+        return maximoDescubierto;
+    }
+
+    @Override
+    public double tipoInteres() {
+        return 0;
+    }
 }
--- src/main/java/org/example/CuentaCorrientePersonal.java
+++ src/main/java/org/example/CuentaCorrientePersonal.java
@@ -1,4 +1,30 @@
 package org.example;
 
 public class CuentaCorrientePersonal extends CuentaCorriente {
+    double comisionMantenimiento;
+
+    public CuentaCorrientePersonal(Persona titular, int saldo, String iban, double comisionMantenimiento) {
+        super(titular, saldo, iban);
+        this.comisionMantenimiento = comisionMantenimiento;
+    }
+
+    @Override
+    public double comisionMantenimiento() {
+        return comisionMantenimiento;
+    }
+
+    @Override
+    public double tipoInteresDescubierto() {
+        return 0;
+    }
+
+    @Override
+    public double maximoDescubierto() {
+        return 0;
+    }
+
+    @Override
+    public double tipoInteres() {
+        return 0;
+    }
 }
