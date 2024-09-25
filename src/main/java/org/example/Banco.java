2a8b4ea7d5237ed168adccd0c8c8e1473bf4e938
carlafber <carla.fueber@educa.jcyl.es>
2024-09-25 14:03:38 +0200

    Commit

--- src/main/java/org/example/Banco.java
+++ src/main/java/org/example/Banco.java
@@ -3,10 +3,14 @@ package org.example;
 import java.util.ArrayList;
 
 public class Banco {
-    ArrayList<CuentaBancaria> lista_cuentas = new ArrayList<>();
+    static ArrayList<CuentaBancaria> lista_cuentas = new ArrayList<>();
     public static boolean abrirCuenta (CuentaBancaria cuenta) {
         lista_cuentas.add(cuenta);
-        for(CuentaBancaria cuentaBancaria : lista_cuentas);
+        for(CuentaBancaria cuentaBancaria : lista_cuentas){
+            if(cuentaBancaria.equals(cuenta)){
+                return true;
+            }
+        }
         return false;
     }
 }
